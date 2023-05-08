a522c9ffb1e3deff92e52ba3edf21d2f769eccc4
Geoff Fite <GFite@users.noreply.github.com>
2023-05-07 20:28:10 -0700

    init

--- start_container.sh
+++ start_container.sh
@@ -3,5 +3,5 @@ alias python=python3
 cat ./FinX.env >> ./BaseConfig.env
 cp ./BaseConfig.env ./.env
 cd ~/FinX-GPT-Environment
-bash run.sh --install-plugin-deps
+pip install -r requirements.txt
 sudo docker run -it --env-file=./.env --network='autogpt-network' -v $PWD/auto_gpt_workspace:/home/appuser/auto_gpt_workspace autogpt
