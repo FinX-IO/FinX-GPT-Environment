f8a96c3d6d3c07f2bcf121730f36339cbfbe573b
Geoff Fite <GFite@users.noreply.github.com>
2023-05-07 18:57:21 -0700

    init

--- start_container.sh
+++ start_container.sh
@@ -1,2 +1,2 @@
 cat ./FinX.env >> ./BaseConfig.env
-sudo docker scripts/run -it --env-file=./BaseConfig.env --network='autogpt-network' -v $PWD/auto_gpt_workspace:/home/appuser/auto_gpt_workspace autogpt
+sudo docker run -it --env-file=./BaseConfig.env --network='autogpt-network' -v $PWD/auto_gpt_workspace:/home/appuser/auto_gpt_workspace autogpt
