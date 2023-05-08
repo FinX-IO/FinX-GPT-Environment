bb8e799188cda99c3f9b420ec766fd8fcf2d545c
Geoff Fite <GFite@users.noreply.github.com>
2023-05-07 19:00:14 -0700

    init

--- start_container.sh
+++ start_container.sh
@@ -1,3 +1,3 @@
 sudo docker build -t autogpt .
 cat ./FinX.env >> ./BaseConfig.env
-sudo docker run -it --install-plugin-deps --env-file=./BaseConfig.env --network='autogpt-network' -v $PWD/auto_gpt_workspace:/home/appuser/auto_gpt_workspace autogpt
+sudo docker run -it --env-file=./BaseConfig.env --network='autogpt-network' -v $PWD/auto_gpt_workspace:/home/appuser/auto_gpt_workspace autogpt
