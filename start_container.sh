a4324d68980addb9549dbaa4b2b0ead5ba2ce2dc
Geoff Fite <GFite@users.noreply.github.com>
2023-05-07 20:06:41 -0700

    init

--- start_container.sh
+++ start_container.sh
@@ -1,10 +1,7 @@
 sudo docker image build -t autogpt .
+alias python=python3
 cat ./FinX.env >> ./BaseConfig.env
-wget "https://github.com/FinX-IO/Auto-GPT-FinX-Plugin" -O ~/Auto-GPT-FinX-Plugin
-cd ~/Auto-GPT-FinX-Plugin
-pip install -r requirements.txt
-cd ~/FinX-GPT-Environment/autogpt/plugins
-zip -r FinX.zip ~/Auto-GPT-FinX-Plugin
-chmod a+rwx FinX.zip
+cp ./BaseConfig.env ./.env
 cd ~/FinX-GPT-Environment
+bash run.sh --install-plugin-deps
 sudo docker run -it --env-file=./.env --network='autogpt-network' -v $PWD/auto_gpt_workspace:/home/appuser/auto_gpt_workspace autogpt
